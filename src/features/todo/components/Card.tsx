868ada4a1c8201b11e8963d472b6d5e1ac287af1
Huydt13 <huydt@bakco.com.vn>
2022-11-08 19:14:25 +0700

    feat : optimize code

--- src/features/todo/components/Card.tsx
+++ src/features/todo/components/Card.tsx
@@ -1,8 +1,21 @@
 import { Button, CardActions, CardContent, Typography } from '@mui/material';
+import Popup from 'components/Common/PopUp';
+import { Todo } from 'models/todo';
 import React from 'react';
 import { data } from '../../../api/dataFake';
+import TodoForm from './TodoForm';
 
 export default function CardCpn({ task, onclick, children, background }: any) {
+  const [openPopup, setOpenPopup] = React.useState(false);
+  const [todo, setTodo] = React.useState<Todo>();
+  const initialValues: Todo = {
+    name: '',
+    ...todo,
+  };
+
+  const handleAdd = () => {
+    setOpenPopup(true);
+  };
   return (
     <div>
       <CardContent style={{ backgroundColor: `${background}`, borderRadius: '5%' }}>
@@ -12,10 +25,20 @@ export default function CardCpn({ task, onclick, children, background }: any) {
       </CardContent>
       {children}
       <CardActions>
-        <Button size="small" onClick={onclick}>
+        <Button size="small" onClick={handleAdd}>
           + Thêm mới
         </Button>
       </CardActions>
+      <Popup
+        title={initialValues?.id ? 'Cập nhật' : 'Tạo mới'}
+        openPopup={openPopup}
+        onClose={() => {
+          setOpenPopup(false);
+          setTodo(undefined);
+        }}
+      >
+        <TodoForm onClose={() => setOpenPopup(false)} />
+      </Popup>
     </div>
   );
 }
--- src/features/todo/components/TodoCard.tsx
+++ src/features/todo/components/TodoCard.tsx
@@ -1,86 +1,49 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Todo } from 'models/todo';
-import Popup from 'components/Common/PopUp';
-import TodoForm from './TodoForm';
 import { data } from '../../../api/dataFake';
 import CardCpn from './Card';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+    display: 'flex',
+    justifyContent: ' space-around',
+  },
+  cardCpn: {
+    border: '1px solid gray',
+    borderRadius: '5%',
+    width: '200px',
+  },
+});
 
 export default function TodoCard() {
-  const [openPopup, setOpenPopup] = React.useState(false);
-  const [todo, setTodo] = React.useState<Todo>();
-  const initialValues: Todo = {
-    name: '',
-    ...todo,
-  };
-
-  const handleAdd = () => {
-    setOpenPopup(true);
-  };
-
+  const classes = useStyles();
   return (
     <>
-      <Card sx={{ minWidth: 275 }} style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <div style={{ border: '1px solid gray', borderRadius: '5%', width: '200px' }}>
+      <Card className={classes.card}>
+        <div className={classes.cardCpn}>
+          <CardCpn
+            task="Cần làm"
+            children={data.filter((e) => e.status === 0).map((e) => e.name)}
+            background="red"
+          />
+        </div>
+        <div className={classes.cardCpn}>
+          <CardCpn
+            task="Cần làm"
+            children={data.filter((e) => e.status === 1).map((e) => e.name)}
+            background="yellow"
+          />
+        </div>
+        <div className={classes.cardCpn}>
           <CardCpn
             task="Cần làm"
             children={data.filter((e) => e.status === 2).map((e) => e.name)}
-            background="red"
-            onclick={handleAdd}
+            background="green"
           />
         </div>
-        <div style={{ border: '1px solid gray', borderRadius: '5%', width: '200px' }}>
-          <CardContent style={{ backgroundColor: '#e2e514', borderRadius: '5%' }}>
-            <Typography variant="h6" component="div">
-              Đang làm
-            </Typography>
-          </CardContent>
-          {data.map((e) => e.name)}
-          <CardActions>
-            <Button size="small" onClick={handleAdd}>
-              + Thêm mới
-            </Button>
-          </CardActions>
-        </div>
-        <div style={{ border: '1px solid gray', borderRadius: '5%', width: '200px' }}>
-          <CardContent style={{ backgroundColor: '#2b593f' }}>
-            <Typography variant="h6" component="div">
-              Đã xong
-            </Typography>
-          </CardContent>
-          {data.filter((e) => e.status === 2).map((e) => e.name)}
-          {console.log(
-            'data',
-            data.filter((e) => e.status === 2).map((e) => e.name)
-          )}
-          <CardActions>
-            <Button size="small" onClick={handleAdd}>
-              + Thêm mới
-            </Button>
-          </CardActions>
-        </div>
-        <CardCpn
-          task="Cần làm"
-          children={data.filter((e) => e.status === 2).map((e) => e.name)}
-          background="red"
-        />
       </Card>
-
-      <Popup
-        title={initialValues?.id ? 'Cập nhật' : 'Tạo mới'}
-        openPopup={openPopup}
-        onClose={() => {
-          setOpenPopup(false);
-          setTodo(undefined);
-        }}
-      >
-        <TodoForm onClose={() => setOpenPopup(false)} />
-      </Popup>
     </>
   );
 }
